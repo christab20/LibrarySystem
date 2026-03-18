0b872cb9c12b5dfca5e9ebe086bcd4bc697514df
christab20 <christabobymon@gmail.com>
2026-03-18 09:41:23 +0000

    Update Book.java

--- Book.java
+++ Book.java
@@ -11,33 +11,48 @@ public class Book extends LibraryItem {
     private String isbn;
 
     // Constructor to create a Book object
+    //Parameters:
+    //title - the name of the book
+    // availiable - indicates if the book is currently available (true/false)
+    // itemId - unique identifier for the book
+    //author - name of the author
+    //isbn - ISBN number of the book
     public Book(String title, boolean available, int itemId, String author, String isbn) {
+        //Calls the constructor of the parent class (LibraryItem)
         super(title, available, itemId);
+
+        //Assign values to Book - specific attributes 
         this.author = author;
         this.isbn = isbn;
     }
 
     // Getter method for the author
+    //Returns the name of the autor
     public String getAuthor() {
         return author;
     }
 
     // Setter method for the author
+    //Allows updating the author's name 
     public void setAuthor(String author) {
         this.author = author;
     }
 
     // Getter method for ISBN
+    //Returns the ISBN number of the book
     public String getIsbn() {
         return isbn;
     }
 
     // Setter method for ISBN 
+    //Allows uodating the ISBN number
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
 
     // toString method
+    //Returns a string representation of the Book object
+    //Includes a;ll important details of the book
     @Override
     public String toString() {
         return "Book{" +
