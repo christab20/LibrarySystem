d8de76508c734c34072ca00d398052ebdfdd16a2
christab20 <christabobymon@gmail.com>
2026-03-18 09:19:23 +0000

    Create LibraryItem.java

---
+++ LibraryItem.java
@@ -0,0 +1,90 @@
+library item package com.mycompany.lms;
+ 
+// LibraryItem.java
+
+public class LibraryItem {
+
+    //Variable to store the title of the library item 
+
+    private String title;
+
+    //Variable to store weether the item is available or not
+
+    private boolean available;
+
+    //Variable to store the unique ID of the item 
+
+    private int itemId;
+
+    //Constructor
+
+    //Parameters 
+
+    //Title of the item 
+
+    //if it is available or not 
+
+    //unique ID number for the Item
+
+ 
+    public LibraryItem(String title, boolean available, int itemId) {
+
+        // Constructor implementation
+
+    }
+ 
+    public String getTitle() {
+
+        // Getter implementation
+
+        return null;
+
+    }
+ 
+    public void setTitle(String title) {
+
+        // Setter implementation
+
+    }
+ 
+    public boolean isAvailable() {
+
+        // Getter implementation
+
+        return false;
+
+    }
+ 
+    public void setAvailable(boolean available) {
+
+        // Setter implementation
+
+    }
+ 
+    public int getItemId() {
+
+        // Getter implementation
+
+        return 0;
+
+    }
+ 
+    public void setItemId(int itemId) {
+
+        // Setter implementation
+
+    }
+ 
+    @Override
+
+    public String toString() {
+
+        // toString implementation
+
+        return null;
+
+    }
+
+}
+
+ 
