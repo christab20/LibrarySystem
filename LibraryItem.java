42ac156435b8c395a42443204fa137975f35c564
christab20 <christabobymon@gmail.com>
2026-03-18 09:48:06 +0000

    Update LibraryItem.java

--- LibraryItem.java
+++ LibraryItem.java
@@ -1,6 +1,9 @@
 library item package com.mycompany.lms;
  
 // LibraryItem.java
+//This class represents a general item in the library 
+// It is the parent class for items like Book, DVD, etc.
+
 
 public class LibraryItem {
 
@@ -16,75 +19,67 @@ public class LibraryItem {
 
     private int itemId;
 
-    //Constructor
-
-    //Parameters 
-
-    //Title of the item 
-
-    //if it is available or not 
-
-    //unique ID number for the Item
-
+// Constructor to create a new LibraryItem object
+    // Parameters:
+    // title - the name of the item
+    // available - indicates if the item is available
+    // itemId - unique identifier for the item
  
     public LibraryItem(String title, boolean available, int itemId) {
 
-        // Constructor implementation
+       //Assign values to instance variables
+       this.title = title;
+       this.availiable = available;
+       this.itemId = itemId
 
     }
  
+   //Getter method for title
+ //Returns the title of the item
     public String getTitle() {
-
-        // Getter implementation
-
-        return null;
+        return title ;
 
     }
- 
+  // Setter method for title
+    // Allows updating the title of the item
     public void setTitle(String title) {
-
-        // Setter implementation
-
+        this.title = title;
     }
- 
+
+    // Getter method for availability
+    // Returns true if the item is available, false otherwise
     public boolean isAvailable() {
-
-        // Getter implementation
-
-        return false;
-
+        return available;
     }
- 
+
+    // Setter method for availability
+    // Allows updating the availability status
     public void setAvailable(boolean available) {
-
-        // Setter implementation
-
+        this.available = available;
     }
- 
+
+    // Getter method for item ID
+    // Returns the unique ID of the item
     public int getItemId() {
-
-        // Getter implementation
-
-        return 0;
-
+        return itemId;
     }
- 
+
+    // Setter method for item ID
+    // Allows updating the item ID
     public void setItemId(int itemId) {
-
-        // Setter implementation
-
+        this.itemId = itemId;
     }
- 
+
+    // toString method
+    // Returns a string representation of the LibraryItem object
+    // Includes key details of the item
     @Override
-
     public String toString() {
-
-        // toString implementation
-
-        return null;
-
+        return "LibraryItem{" +
+               "title='" + title + '\'' +
+               ", available=" + available +
+               ", itemId=" + itemId +
+               '}';
     }
-
 }
-
- 
+  
