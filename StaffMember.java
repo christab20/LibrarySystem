ed20dbb9cff268672b0d978ed016eb7cc6067fd9
christab20 <christabobymon@gmail.com>
2026-03-20 09:46:01 +0000

    Update StaffMember.java

--- StaffMember.java
+++ StaffMember.java
@@ -3,36 +3,61 @@ package com.mycompany.lms;
 
 
 // StaffMember.java
+//This class represents a staff member in the library system 
+// It extends the Member class and defines a higher borrowing limit
+
 public class StaffMember extends Member {
+    // Maximum number of books a staff member can borrow
     private int maxBooks = 10;
 
+    // Constructor to create a StaffMember object
+    // Parameters:
+    // memberId - unique ID of the member
+    // name - name of the member
+    // email - email address of the member
+    // borrowedBooks - number of books currently borrowed
     public StaffMember(int memberId, String name, String email, int borrowedBooks) {
+        // Call parent class constructor
         super(memberId, name, email, borrowedBooks);
     }
 
+    // Getter method for maxBooks
+    // Returns the maximum borrowing limit for staff
     @Override
     public int getMaxBooks() {
         return maxBooks;
     }
 
+    // Setter method for maxBooks
+    // Allows updating the borrowing limit
     public void setMaxBooks(int maxBooks) {
         this.maxBooks = maxBooks;
     }
 
+     // Method to increment the number of borrowed books
+    // Ensures staff does not exceed their borrowing limit
      @Override
     public void incrementBorrowedBooks() {
         if (getBorrowedBooks() < maxBooks) {
+            // Call parent method to increase borrowedBooks
             super.incrementBorrowedBooks();
         } else {
+            // Display message if limit is reached
             System.out.println("Cannot borrow more books! Limit reached.");
         }
     }
+    // Method to decrement the number of borrowed books
+    // Ensures the count does not go below zero
     @Override
     public void decrementBorrowedBooks() {
         if (getBorrowedBooks() > 0) {
+             // Call parent method to decrease borrowedBooks
             super.decrementBorrowedBooks();
         }
     }
+    // toString method
+    // Returns a string representation of the StaffMember object
+    // Includes all relevant member details and borrowing limit
     @Override
     public String toString() {
         return "StaffMember{" +
